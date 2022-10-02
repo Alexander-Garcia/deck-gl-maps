62a5252235a33a79e1d5b78cd7095f56f9709c7d
Alexander <31865557+Alexander-Garcia@users.noreply.github.com>
2022-10-02 00:28:00 -0500

    feat: added geojson layer for iowa census tract data

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,17 +1,31 @@
-import DeckGL from 'deck.gl';
+import DeckGL, { GeoJsonLayer } from 'deck.gl';
 import { Map as StaticMap } from 'react-map-gl';
+import data from '../data/iowaData.json';
 
 const initial = {
-  longitude: -122.41669,
-  latitude: 37.7853,
-  zoom: 13,
+  longitude: -93.598022,
+  latitude: 41.619549,
+  zoom: 6,
   pitch: 0,
   bearing: 0,
 };
 
+const layer = new GeoJsonLayer({
+  id: 'geojson-layer',
+  data,
+  pickable: true,
+  stroked: true,
+  filled: false,
+  lineWidthScale: 20,
+  lineWidthMinPixels: 2,
+  getLineColor: [255, 149, 200],
+  getPointRadius: 100,
+  getLineWidth: 1,
+});
+
 function Map() {
   return (
-    <DeckGL controller initialViewState={initial}>
+    <DeckGL controller initialViewState={initial} layers={[layer]}>
       <StaticMap mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN} mapStyle="mapbox://styles/mapbox/streets-v9" />
     </DeckGL>
   );
